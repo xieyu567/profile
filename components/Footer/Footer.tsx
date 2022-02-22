b6d07085eff62857469b7af02c4526d4b930df78
xieyu567 <xieyu345800@sina.com>
2022-02-22 14:49:44 +0800

    test

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from './FooterStyles';
 
 const Footer = () => {
   return (
@@ -10,11 +20,11 @@ const Footer = () => {
       <LinkList>
         <LinkColumn>
           <LinkTitle>Call</LinkTitle>
-          <LinkItem href="tel:314-343-3432">314-343-3432</LinkItem>
+          <LinkItem href='tel:314-343-3432'>314-343-3432</LinkItem>
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
-          <LinkItem href="mailto:contact@jsmastery.com">
+          <LinkItem href='mailto:contact@jsmastery.com'>
             contact@jsmastery.com
           </LinkItem>
         </LinkColumn>
@@ -24,14 +34,14 @@ const Footer = () => {
           <Slogan>Innovating one project at a time</Slogan>
         </CompanyContainer>
         <SocialContainer>
-          <SocialIcons href="https://google.com">
-            <AiFillGithub size="3rem" />
+          <SocialIcons href='https://google.com'>
+            <AiFillGithub size='3rem' />
           </SocialIcons>
-          <SocialIcons href="https://google.com">
-            <AiFillLinkedin size="3rem" />
+          <SocialIcons href='https://google.com'>
+            <AiFillLinkedin size='3rem' />
           </SocialIcons>
-          <SocialIcons href="https://google.com">
-            <AiFillInstagram size="3rem" />
+          <SocialIcons href='https://google.com'>
+            <AiFillInstagram size='3rem' />
           </SocialIcons>
         </SocialContainer>
       </SocialIconsContainer>
--- components/Layout/layout.tsx
+++ components/Layout/layout.tsx
@@ -1,19 +1,18 @@
-import React, {ReactNode} from 'react';
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
-import { Container } from './LayoutStyles'
+import React, { ReactNode } from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import { Container } from './LayoutStyles';
 
 type Props = {
-    children: ReactNode;
+  children: ReactNode;
 };
 
-
-const Layout = ({children}: Props) => (
-    <Container>
-        <Header/>
-        <main>{children}</main>
-        <Footer/>
-    </Container>
-)
+const Layout = ({ children }: Props) => (
+  <Container>
+    <Header />
+    <main>{children}</main>
+    <Footer />
+  </Container>
+);
 
 export default Layout;
